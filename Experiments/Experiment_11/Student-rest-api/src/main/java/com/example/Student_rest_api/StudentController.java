3573745c9fcf122b14dd448d34585d74dd97fb4a
VedisVigourous <vadanta592007@hotmail.com>
2026-05-24 21:56:49 +0530

    Experiment 11 Added

--- Experiments/Experiment_11/Student-rest-api/src/main/java/com/example/Student_rest_api/StudentController.java
+++ Experiments/Experiment_11/Student-rest-api/src/main/java/com/example/Student_rest_api/StudentController.java
@@ -12,7 +12,8 @@ public class StudentController {
 	
 	@GetMapping
 	 public String welcomeMessage() {
-	 return "Welcome to Student Management REST API - built by Vadanta Kumar Chauhaan";
+	 return "Welcome to Student Management REST API :: Uttam Upadhyay :: CSE-22";
+	 
 	 }
 	
 	@GetMapping("/student")
